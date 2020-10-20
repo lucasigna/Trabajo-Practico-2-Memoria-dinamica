f146e1495406309b61be2cfe944fa846043a39b6
lucasigna <lucasignaciogonzalez1999@gmail.com>
2020-10-20 18:29:40 -0300

    Ejercicio 35

---
+++ Ejercicio 35/header.h
@@ -0,0 +1,17 @@
+#ifndef MY_LIB
+#define MY_LIB
+#include <stdio.h>
+
+typedef struct persona_pila{
+        char nombre[30];
+        char apellido[30];
+        int edad;
+        long telefono;
+        char mail[100];
+        struct persona_pila * lazo;
+        }persona;
+
+void escribir(void);
+void mostrar(void);
+
+#endif
